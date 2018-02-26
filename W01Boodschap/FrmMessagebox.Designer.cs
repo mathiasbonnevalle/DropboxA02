84b7bf1e6c9abbcfdb3c99c6b1df104839525b0b
Mathias Bonnevalle <mathias.bonnevalle@student.howest.be>
2018-02-26 19:39:10 +0100

    W01Boodschap commit

---
+++ W01Boodschap/FrmMessagebox.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace W01Boodschap
+{
+    partial class FrmMessagebox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVenster1 = new System.Windows.Forms.Button();
+            this.btnVenster2 = new System.Windows.Forms.Button();
+            this.btnVenster3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVenster1
+            // 
+            this.btnVenster1.Location = new System.Drawing.Point(56, 100);
+            this.btnVenster1.Name = "btnVenster1";
+            this.btnVenster1.Size = new System.Drawing.Size(100, 50);
+            this.btnVenster1.TabIndex = 0;
+            this.btnVenster1.Text = "Venster 1";
+            this.btnVenster1.UseVisualStyleBackColor = true;
+            this.btnVenster1.Click += new System.EventHandler(this.btnVenster1_Click);
+            // 
+            // btnVenster2
+            // 
+            this.btnVenster2.Location = new System.Drawing.Point(254, 100);
+            this.btnVenster2.Name = "btnVenster2";
+            this.btnVenster2.Size = new System.Drawing.Size(100, 50);
+            this.btnVenster2.TabIndex = 1;
+            this.btnVenster2.Text = "Venster 2";
+            this.btnVenster2.UseVisualStyleBackColor = true;
+            this.btnVenster2.Click += new System.EventHandler(this.btnVenster2_Click);
+            // 
+            // btnVenster3
+            // 
+            this.btnVenster3.Location = new System.Drawing.Point(475, 100);
+            this.btnVenster3.Name = "btnVenster3";
+            this.btnVenster3.Size = new System.Drawing.Size(100, 50);
+            this.btnVenster3.TabIndex = 2;
+            this.btnVenster3.Text = "Venster 3";
+            this.btnVenster3.UseVisualStyleBackColor = true;
+            this.btnVenster3.Click += new System.EventHandler(this.btnVenster3_Click);
+            // 
+            // FrmMessagebox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 226);
+            this.Controls.Add(this.btnVenster3);
+            this.Controls.Add(this.btnVenster2);
+            this.Controls.Add(this.btnVenster1);
+            this.Name = "FrmMessagebox";
+            this.Text = "FrmMessagebox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVenster1;
+        private System.Windows.Forms.Button btnVenster2;
+        private System.Windows.Forms.Button btnVenster3;
+    }
+}
---
+++ W01Boodschap/FrmMessagebox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W01Boodschap
+{
+    public partial class FrmMessagebox : Form
+    {
+        public FrmMessagebox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVenster1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Uw verzoek zal behandeld worden." + Environment.NewLine + 
+                "Wil je verder werken?", " ", MessageBoxButtons.YesNo, MessageBoxIcon.Question
+                , MessageBoxDefaultButton.Button1);
+        }
+
+        private void btnVenster2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dit artikel is niet meer op voorraad!", "Voorraadcontrole", MessageBoxButtons.OK,
+                MessageBoxIcon.Stop, MessageBoxDefaultButton.Button1);
+        }
+
+        private void btnVenster3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dit is niet gelukt!" + Environment.NewLine + "Opnieuw?",
+                "Pas op!", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+        }
+    }
+}
